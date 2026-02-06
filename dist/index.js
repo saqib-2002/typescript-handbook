47b8df2d2da9f87531e0668845d9bec3100027ee
Saqib <saqibsaifi720@gmail.com>
2026-02-06 16:35:05 +0530

    learned about type annotations, type alias, interface, literal types

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,64 @@
-const func = (user, gender) => {
-    const userInfo = {
-        id: user.id,
-        name: user.name,
-        gender: typeof gender === "string" ? gender : gender === true ? "male" : "female",
-    };
-    return userInfo;
+// Type Annotations
+// Function Types
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-console.log(func({ name: "Helen", id: 12 }, false));
+const greet = (person, date) => {
+    // parameter type annotations on the function parameters and return type annotations appear after the function parameters list.
+    const msg = `Hello ${person}, today is ${date.toDateString()}`;
+    return msg;
+};
+console.log(greet("Maddison", new Date()));
+// function which returns promises
+const timeout = () => __awaiter(void 0, void 0, void 0, function* () {
+    const time = setTimeout(() => {
+        console.log("promise runs after 5sec");
+    }, 5000);
+    return time;
+});
+timeout();
+const names = ["Alice", "Bob", "Eve"];
+// contextual typing, parameter s is string as the forEach method informs the type annotation
+names.forEach((s) => {
+    console.log(s.toUpperCase());
+});
+const printCoord = (pt) => {
+    typeof pt.x === "string" // narrowing the union
+        ? console.log("The coordinate of x is in string and is " + pt.x)
+        : console.log("The coordinate's x value is " + pt.x);
+    console.log("The coordinate's y value is " + (pt.y || 0));
+};
+printCoord({ x: 3, y: 7 });
+printCoord({ x: "two" });
+const welcomePeople = (x) => {
+    if (Array.isArray(x)) {
+        // Here: 'x' is 'string[]'
+        console.log("Hello, " + x.join(" and "));
+    }
+    else {
+        // Here: 'x' is 'string'
+        console.log("Welcome lone traveler " + x);
+    }
+};
+welcomePeople(["saqib", "saifi", "saqsa"]);
+//Literal Types
+let str = "Hello";
+str = "Hello";
+// str = "Bye"; // error, Type '"Bye"' is not assignable to type '"Hello"'
+const obj = {
+    counter: 2,
+};
+console.log((obj.counter = 10));
+// const req = { url: "https://example.com", method: "GET" };
+// const req = { url: "https://example.com", method: "GET" } as const;
+const req = { url: "https://example.com", method: "GET" };
+handleRequest(req.url, req.method);
 export {};
+// handleRequest(req.url, req.method as "GET");
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,89 @@
-interface User {
-  name: string;
-  id: number;
-}
+// Type Annotations
+// Function Types
 
-const func = (user: User, gender: string | boolean): object => {
-  const userInfo = {
-    id: user.id,
-    name: user.name,
-    gender:
-      typeof gender === "string" ? gender : gender === true ? "male" : "female",
-  };
-  return userInfo;
+const greet = (person: string, date: Date): string => {
+  // parameter type annotations on the function parameters and return type annotations appear after the function parameters list.
+  const msg = `Hello ${person}, today is ${date.toDateString()}`;
+  return msg;
 };
 
-console.log(func({ name: "Helen", id: 12 }, false));
+console.log(greet("Maddison", new Date()));
+
+// function which returns promises
+const timeout = async (): Promise<number> => {
+  const time = setTimeout(() => {
+    console.log("promise runs after 5sec");
+  }, 5000);
+
+  return time;
+};
+
+timeout();
+
+const names = ["Alice", "Bob", "Eve"];
+// contextual typing, parameter s is string as the forEach method informs the type annotation
+names.forEach((s) => {
+  console.log(s.toUpperCase());
+});
+
+// Object Types and Union Types
+// y property is optional by adding the ?.
+
+// const printCoord = (pt: { x: number | string; y?: number }) => {
+
+// Type alias
+
+// type Point = {
+//   x: number | string;
+//   y?: number;
+// };
+
+// An interface declaration is another way to name an object type
+// type cannot be re-opened to add new properties vs an interface which is always extendable.
+interface Point {
+  x: number | string;
+  y?: number;
+}
+
+const printCoord = (pt: Point) => {
+  typeof pt.x === "string" // narrowing the union
+    ? console.log("The coordinate of x is in string and is " + pt.x)
+    : console.log("The coordinate's x value is " + pt.x);
+
+  console.log("The coordinate's y value is " + (pt.y || 0));
+};
+printCoord({ x: 3, y: 7 });
+printCoord({ x: "two" });
+
+const welcomePeople = (x: string[] | string) => {
+  if (Array.isArray(x)) {
+    // Here: 'x' is 'string[]'
+    console.log("Hello, " + x.join(" and "));
+  } else {
+    // Here: 'x' is 'string'
+    console.log("Welcome lone traveler " + x);
+  }
+};
+
+welcomePeople(["saqib", "saifi", "saqsa"]);
+
+//Literal Types
+
+let str: "Hello" = "Hello";
+
+str = "Hello";
+// str = "Bye"; // error, Type '"Bye"' is not assignable to type '"Hello"'
+
+const obj = {
+  counter: 2,
+};
+
+console.log((obj.counter = 10));
+
+declare function handleRequest(url: string, method: "GET" | "POST"): void;
+
+// const req = { url: "https://example.com", method: "GET" };
+// const req = { url: "https://example.com", method: "GET" } as const;
+const req = { url: "https://example.com", method: "GET" as "GET" };
+handleRequest(req.url, req.method);
+// handleRequest(req.url, req.method as "GET");
